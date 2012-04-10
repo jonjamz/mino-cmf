90008d571a08bfa401355ce061322edf761ec155
Jonathan James <jonjamz86@gmail.com>
2012-04-10 03:46:27 -0700

    updated model router code, just added a comment

--- routers/model.router.php
+++ routers/model.router.php
@@ -2,8 +2,8 @@
 
   $modelDir = __DIR__."/../server/models/";
 
+  // Add paths to models here
   $models = array(
-
     "activate"    => "auth/activate.php",
     "login"       => "auth/login.php",
     "register"    => "auth/register.php",
@@ -11,15 +11,12 @@
     "settings"    => "settings.php",
     "dashboard"   => "dashboard.php",
     "searches"    => "searches.php"
-
   );
 
   if(isset($_POST['model'])) {
-
     $model = $_POST['model'];
     $destination = $modelDir.$models[$model];
     require $destination;
-
   }
 
 ?>
