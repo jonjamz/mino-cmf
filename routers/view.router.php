8fb062547c2bab779574cabd96041d16046a7dd5
Jonathan James <jonjamz86@gmail.com>
2012-04-10 03:44:21 -0700

    updated views and view router (removed --DEFAULT folder like I did with models)

--- routers/view.router.php
+++ routers/view.router.php
@@ -18,7 +18,6 @@
   /* OPTIONS
    * ========================== */
 
-
   // Doesn't require __DIR__ because the locations go to js for referencing
   $viewDir = "client/views/";
 
@@ -34,17 +33,16 @@
   /* ROUTES
    * ========================== */
 
-
   // Universally accessible
   $uViews = array(
 
-    "company"     => array("title" => "Company", "location" => "--DEFAULT/company.html", "parent" => ""),
-      "company/about"       => array("title" => "About Us", "location" => "--DEFAULT/about.html", "parent" => "company"),
-      "company/help"        => array("title" => "Help", "location" => "--DEFAULT/help.html", "parent" => "company"),
-      "company/terms"       => array("title" => "Terms", "location" => "--DEFAULT/terms.html", "parent" => "company"),
-      "company/contact"     => array("title" => "Contact Us", "location" => "--DEFAULT/contact.html", "parent" => "company"),
-    "uploads"     => array("title" => "Uploads", "location" => "--DEFAULT/uploads.html", "parent" => ""),
-    "404"         => array("title" => "404 Error -- File Not Found", "location" => "--DEFAULT/404.html", "parent" => "")
+    "company"     => array("title" => "Company", "location" => "company.html", "parent" => ""),
+      "company/about"       => array("title" => "About Us", "location" => "about.html", "parent" => "company"),
+      "company/help"        => array("title" => "Help", "location" => "help.html", "parent" => "company"),
+      "company/terms"       => array("title" => "Terms", "location" => "terms.html", "parent" => "company"),
+      "company/contact"     => array("title" => "Contact Us", "location" => "contact.html", "parent" => "company"),
+    "uploads"     => array("title" => "Uploads", "location" => "uploads.html", "parent" => ""),
+    "404"         => array("title" => "404 Error -- File Not Found", "location" => "404.html", "parent" => "")
 
   );
 
@@ -52,11 +50,11 @@
   // Only when logged out
   $oViews = array(
 
-    "home"        => array("title" => "Welcome Home", "location" => "--DEFAULT/home.html", "parent" => ""),
-      "register"    => array("title" => "Registration Page", "location" => "--DEFAULT/authorization/register.html", "parent" => "home"),
-    "activate"    => array("title" => "Activate Your Account", "location" => "--DEFAULT/authorization/activate.html", "parent" => ""),
-    "change-pass" => array("title" => "Change Your Password", "location" => "--DEFAULT/authorization/forgot.html", "parent" => ""),
-    "login"       => array("title" => "Login Page", "location" => "--DEFAULT/authorization/login.html", "parent" => "")
+    "home"        => array("title" => "Welcome Home", "location" => "home.html", "parent" => ""),
+      "register"    => array("title" => "Registration Page", "location" => "auth/register.html", "parent" => "home"),
+    "activate"    => array("title" => "Activate Your Account", "location" => "auth/activate.html", "parent" => ""),
+    "change-pass" => array("title" => "Change Your Password", "location" => "auth/forgot.html", "parent" => ""),
+    "login"       => array("title" => "Login Page", "location" => "auth/login.html", "parent" => "")
 
   );
 
@@ -64,9 +62,9 @@
   // Only when logged in
   $iViews = array(
 
-    "dashboard"   => array("title" => "Dashboard", "location" => "--DEFAULT/user/dashboard.html", "parent" => ""),
-    "settings"    => array("title" => "Account Settings", "location" => "--DEFAULT/user/settings.html", "parent" => "dashboard"),
-    "profile"     => array("title" => "Profile", "location" => "--DEFAULT/user/profile.html", "parent" => "dashboard"),
+    "dashboard"   => array("title" => "Dashboard", "location" => "user/dashboard.html", "parent" => ""),
+    "settings"    => array("title" => "Account Settings", "location" => "user/settings.html", "parent" => "dashboard"),
+    "profile"     => array("title" => "Profile", "location" => "user/profile.html", "parent" => "dashboard"),
     "mypage"      => array("title" => "My Page", "location" => "mypage.html", "parent" => "dashboard")
 
   );
@@ -100,63 +98,39 @@
 
   // If there is nothing (it's the index) route to logged in or out home page
   if($urlMe == 'emptyVar') {
-
         if(isset($_SESSION['id'])) {
-
           $view = 'dashboard';
-
-        } else {
-
-          $view = 'home';
-
-        }
+        } 
+        else { $view = 'home'; }
 
   // For every other case...
-  } else {
-
+  } 
+  else {
     // First check for URL arguments
     if(preg_match('/~/', $urlMe) != 0) {
-
       // Separate URL args from page name and create $view
       $url = explode('~', $urlMe);
       $view = $url[0];
-
         // Check if there is more than one variable.
         if(preg_match('/,/', $url[1]) != 0) {
-
           // Split up variables by &
           $args = explode(',', $url[1]);
-
-        } else {
-
-          $args = array($url[1]);
-
-        }
-
+        } 
+        else { $args = array($url[1]); }
         // Assign args to an array
         foreach($args as $arg) {
-
             // If there's an equals sign, go associative!
             if(preg_match('/=/', $arg) != 0) {
-
               $kv = explode('=', $arg);
               $urlArgs[$kv[0]] = $kv[1];
-
             }
-
             // If there is no equals sign, go numeric!
             elseif(preg_match('/=/', $arg) === 0) {
-
               $urlArgs[] = $arg;
-
             }
         }
-
-    } else {
-
-      $view = $urlMe;
-
-    }
+    } 
+    else { $view = $urlMe; }
   }
 
 
@@ -166,96 +140,74 @@
   */
 
   function createViewInfo($view, $flag, $viewDir, $uViews, $iViews, $oViews, $is404, $isTitles) {
-
     if(isset($uViews[$view])) {
-
       // Location
       $viewLoc = $viewDir.$uViews[$view]['location'];
-
       // Parent
       $parent = $uViews[$view]['parent'];
-
       // Title
       $title = $uViews[$view]['title'];
-
-    } elseif(isset($oViews[$view]['location'])) {
-
+    } 
+    elseif(isset($oViews[$view]['location'])) {
         if(isset($_SESSION['id'])) {
-
           // Redirect out/in
           $flag = 2;
           $viewLoc = "!log";
-
-        } else {
-
+        } 
+        else {
           $viewLoc = $viewDir.$oViews[$view]['location'];
           $parent = $oViews[$view]['parent'];
           $title = $oViews[$view]['title'];
-
         }
-
-    } elseif(isset($iViews[$view])) {
-
+    } 
+    elseif(isset($iViews[$view])) {
         if(isset($_SESSION['id'])) {
-
           $viewLoc = $viewDir.$iViews[$view]['location'];
           $parent = $iViews[$view]['parent'];
           $title = $iViews[$view]['title'];
-
-        } else {
-
+        } 
+        else {
           // Redirect out/in
           $viewLoc = '!log';
-
         }
-
-    } else {
+    } 
+    else {
 
 
       /*
           When $view doesn't match any existing views...404
       */
 
-
       // Modify init flag to perform a full redirect in the controller. If 404 mode is enabled, go to 404
       if($flag === 1) {
-
         // These are handled differently by the controller, so they send a different type of response
         $flag = 2;
-        if($is404 == "yes") { $viewLoc = "/404"; } elseif($is404 == "no") { $viewLoc = "/"; }
-
-      } elseif($flag === 0) {
-
+        if($is404 == "yes") { $viewLoc = "/404"; } 
+        elseif($is404 == "no") { $viewLoc = "/"; }
+      } 
+      elseif($flag === 0) {
           if(isset($_SESSION['id'])) {
-
               if($is404 == "yes") {
-
                 $view = '404';
                 $viewLoc = $viewDir.$uViews['404']['location'];
                 $title = $viewDir.$uViews['404']['title'];
-
-              } else {
-
+              } 
+              else {
                 $view = 'dashboard';
                 $viewLoc = $viewDir.$iViews['dashboard']['location'];
                 $title = $viewDir.$iViews['dashboard']['title'];
-
               }
-
-          } else {
-
+          } 
+          else {
               if($is404 == "yes") {
-
                 $view = '404';
                 $viewLoc = $viewDir.$uViews['404']['location'];
                 $title = $viewDir.$uViews['404']['title'];
-
-              } else {
-
+              } 
+              else {
                 $view = 'home';
                 $viewLoc = $viewDir.$oViews['home']['location'];
                 $title = $viewDir.$oViews['home']['title'];
-
               }
           }
       }
@@ -268,30 +220,18 @@
     */
 
     if($isTitles == "yes") {
-
-
         if(isset($view)) {
-
           $title = preg_replace('/^.*(\\/)/', '', $view);
           $title = ucfirst($title);
-
-        } else {
-
+        } 
+        else {
           // This is when flag is 2, doesn't matter but I guess we should return something
           $title = ' ';
-
         }
-
-    } else {
-
-        if($flag === 2) {
-
-          $title = ' ';
-
-        }
+    } 
+    else {
+        if($flag === 2) { $title = ' '; }
     }
-
-
     // Put in an array
     return  array(
                       "view"    => $viewLoc,
@@ -300,39 +240,27 @@
                       "flag"    => $flag,
                       "parent"  => $parent
     );
-
   }
-
 
   // Create view info
   $firstView = createViewInfo($view, $flag, $viewDir, $uViews, $iViews, $oViews, $is404, $isTitles);
-
   // Deal with parents
   if($firstView['parent'] != "") {
-
     // Have to run this through the above function again
     $firstParent = createViewInfo($firstView['parent'], $flag, $viewDir, $uViews, $iViews, $oViews, $is404, $isTitles);
-
     // Create array for encoding
     $final = array(
-
                       "parent"  => $firstParent,
                       "view"    => $firstView,
                       "vars"    => $urlArgs
-
                   );
-
-  } else {
-
+  } 
+  else {
     $final = array(
-
                       "view"    => $firstView,
                       "vars"    => $urlArgs
-
                   );
-
   }
-
 
   // JSON-encode the array and return it
   echo json_encode($final);
